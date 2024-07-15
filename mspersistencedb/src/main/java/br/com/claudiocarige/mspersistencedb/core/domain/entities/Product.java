92c14dbb83f4ed176ed93e0eb92153ef165d7ddc
Claudio Carigé <ccarige@gmail.com>
2024-07-15 15:14:50 -0300

    [ addition ] : Created Product class

---
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/domain/entities/Product.java
@@ -0,0 +1,46 @@
+package br.com.claudiocarige.mspersistencedb.core.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    private String name;
+
+    private double weight;
+
+    private double volume;
+
+    @Column( precision = 10, scale = 2 )
+    private BigDecimal value;
+
+    @Override
+    public boolean equals( Object o ) {
+
+        if( this == o ) return true;
+        if( o == null || getClass() != o.getClass() ) return false;
+        Product product = ( Product ) o;
+        return Objects.equals( id, product.id ) && Objects.equals( name, product.name );
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash( id, name );
+    }
+
+}
