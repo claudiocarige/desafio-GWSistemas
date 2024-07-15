371edd4d12e51a79ab3a334743b5495bbff3c09b
Claudio Carigé <ccarige@gmail.com>
2024-07-15 12:08:27 -0300

    [ refactor ] : Refactored Customer class

--- mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/domain/Customers.java
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/domain/Customers.java
@@ -1,13 +1,22 @@
 package br.com.claudiocarige.mspersistencedb.core.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public abstract class Customers implements Serializable {
 
@@ -18,21 +27,50 @@ public abstract class Customers implements Serializable {
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
 
+    @Column( unique = true )
     private String customerName;
 
     @OneToOne
     @JoinColumn( name = "address_id" )
     private Address address;
 
+    @Column( unique = true )
     private String phoneNumber;
 
     private String whatsapp;
 
+    @Column( unique = true )
     private String principalEmail;
 
     private String responsibleEmployee;
 
     @ElementCollection( fetch = FetchType.EAGER )
     private List< String > emailList = new ArrayList<>();
+
+
+    public void addEmailInList( String email ) {
+
+        this.emailList.add( email );
+    }
+
+    public void removeEmailInList( String email ) {
+
+        this.emailList.remove( email );
+    }
+
+    @Override
+    public boolean equals( Object o ) {
+
+        if( this == o ) return true;
+        if( o == null || getClass() != o.getClass() ) return false;
+        Customers customers = ( Customers ) o;
+        return Objects.equals( id, customers.id ) && Objects.equals( customerName, customers.customerName ) && Objects.equals( principalEmail, customers.principalEmail );
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash( id, customerName, principalEmail );
+    }
 
 }
