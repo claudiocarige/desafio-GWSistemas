976e161e5de982af867b6f4f815e63a630df04a0
Claudio Carigé <ccarige@gmail.com>
2024-07-15 11:52:58 -0300

    [ addition ] : Inserted variables in the Customers class

--- mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/domain/Customers.java
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/domain/Customers.java
@@ -1,18 +1,38 @@
 package br.com.claudiocarige.mspersistencedb.core.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 public abstract class Customers implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
+
+    private String customerName;
+
+    @OneToOne
+    @JoinColumn( name = "address_id" )
+    private Address address;
+
+    private String phoneNumber;
+
+    private String whatsapp;
+
+    private String principalEmail;
+
+    private String responsibleEmployee;
+
+    @ElementCollection( fetch = FetchType.EAGER )
+    private List< String > emailList = new ArrayList<>();
 
 }
