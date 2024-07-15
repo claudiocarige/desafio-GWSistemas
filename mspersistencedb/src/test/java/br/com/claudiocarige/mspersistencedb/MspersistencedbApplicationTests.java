622729194ac68c1ea29ea1079e0cb62fd08c0618
Claudio Carigé <ccarige@gmail.com>
2024-07-15 11:05:54 -0300

    First commt - Created a new project mspersistencedb

---
+++ mspersistencedb/src/test/java/br/com/claudiocarige/mspersistencedb/MspersistencedbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.claudiocarige.mspersistencedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MspersistencedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
