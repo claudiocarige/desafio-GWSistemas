b26b1148ed94a149b7cc193748beb03d29deefd1
Claudio Carigé <ccarige@gmail.com>
2024-07-16 11:35:19 -0300

    [ addition ] : Custom IllegalArgumentException error class created

---
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/exceptions/IllegalArgumentException.java
@@ -0,0 +1,10 @@
+package br.com.claudiocarige.mspersistencedb.core.exceptions;
+
+public class IllegalArgumentException extends RuntimeException {
+
+    public IllegalArgumentException( String message ) {
+
+        super( message );
+    }
+
+}
