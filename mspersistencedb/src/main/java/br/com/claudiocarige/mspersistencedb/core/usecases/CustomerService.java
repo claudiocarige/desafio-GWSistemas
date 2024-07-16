bd23cb0e0f44d0ad342db50c215bc6eb8800ce79
Claudio Carigé <ccarige@gmail.com>
2024-07-16 12:51:46 -0300

    [ addition ] : created CustomerService interface

---
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/usecases/CustomerService.java
@@ -0,0 +1,24 @@
+package br.com.claudiocarige.mspersistencedb.core.usecases;
+
+import br.com.claudiocarige.mspersistencedb.core.dtos.CompanyCustomerDTO;
+import br.com.claudiocarige.mspersistencedb.core.dtos.CustomerResponseDTO;
+import br.com.claudiocarige.mspersistencedb.core.dtos.IndividualCustomerDTO;
+
+import java.util.List;
+
+
+public interface CustomerService {
+
+    CustomerResponseDTO createIndividualCustomer( IndividualCustomerDTO individualCustomerDTO );
+
+    CustomerResponseDTO createCompanyCustomer( CompanyCustomerDTO companyCustomerDTO);
+
+    CustomerResponseDTO updateIndividualCustomer( IndividualCustomerDTO individualCustomerDTO);
+
+    CustomerResponseDTO updateCompanyCustomer( CompanyCustomerDTO companyCustomerDTO);
+
+    CustomerResponseDTO findCustomerById( Long customerId);
+
+    List<CustomerResponseDTO> listAllCustomers();
+
+}
