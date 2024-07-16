274aea8a5a63835caa4f2e4a03f3514d6214a72a
Claudio Carigé <ccarige@gmail.com>
2024-07-16 11:34:05 -0300

    [ addition ] : Created StandardError erro class

---
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/exceptions/StandardError.java
@@ -0,0 +1,24 @@
+package br.com.claudiocarige.mspersistencedb.core.exceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StandardError {
+
+    private Long timestamp;
+
+    private Integer status;
+
+    private String message;
+
+    private String path;
+
+}
