645c132ea393b20a60e69e553899c0b7dc842d8b
Claudio Carigé <ccarige@gmail.com>
2024-07-17 18:03:09 -0300

    [ refactor ] : Change table creation script

--- mspersistencedb/src/main/resources/db/migration/V1__Create__Table.sql
+++ mspersistencedb/src/main/resources/db/migration/V1__Create__Table.sql
@@ -1,64 +1,62 @@
-CREATE TABLE address (
-                         id BIGSERIAL PRIMARY KEY,
-                         street VARCHAR(255),
-                         number VARCHAR(255),
+CREATE TABLE Address (
+                         id SERIAL PRIMARY KEY,
                          city VARCHAR(255),
-                         state VARCHAR(255),
                          country VARCHAR(255),
-                         postal_code VARCHAR(255),
                          latitude_coordinate VARCHAR(255),
                          longitude_coordinate VARCHAR(255),
-                         customer_id BIGINT
+                         "number" VARCHAR(255),
+                         postal_code VARCHAR(255),
+                         state VARCHAR(255),
+                         street VARCHAR(255)
 );
 
-CREATE TABLE customers (
-                           id BIGSERIAL PRIMARY KEY,
+CREATE TABLE Customers (
+                           id SERIAL PRIMARY KEY,
                            address_id BIGINT UNIQUE,
-                           customer_name VARCHAR(255) UNIQUE,
-                           phone_number VARCHAR(255) UNIQUE,
-                           whatsapp VARCHAR(255),
-                           principal_email VARCHAR(255) UNIQUE,
-                           responsible_employee VARCHAR(255),
                            cpf VARCHAR(14) UNIQUE,
                            cnpj VARCHAR(18) UNIQUE,
-                           dtype VARCHAR(31) NOT NULL
-
+                           dtype VARCHAR(31) NOT NULL,
+                           corporate_name VARCHAR(255),
+                           customer_name VARCHAR(255) UNIQUE,
+                           phone_number VARCHAR(255) UNIQUE,
+                           principal_email VARCHAR(255) UNIQUE,
+                           responsible_employee VARCHAR(255),
+                           whatsapp VARCHAR(255)
 );
 
-CREATE TABLE customers_email_list (
+CREATE TABLE Customers_Email_List (
                                       customers_id BIGINT NOT NULL,
                                       email_list VARCHAR(255)
-
 );
 
-CREATE TABLE delivery (
-                          id BIGSERIAL PRIMARY KEY,
-                          product_id BIGINT,
+CREATE TABLE Delivery (
+                          id SERIAL PRIMARY KEY,
+                          date_solicitation DATE,
                           recipient_id BIGINT,
                           sender_id BIGINT,
-                          is_delivered VARCHAR(255) CHECK (is_delivered IN ('PENDING','DELIVERED','CANCELED'))
-
+                          password_delivery VARCHAR(255) UNIQUE,
+                          status_delivery VARCHAR(255) CHECK (status_delivery IN ('PENDING','DELIVERED','CANCELED'))
 );
 
-CREATE TABLE product (
-                         id BIGSERIAL PRIMARY KEY,
+CREATE TABLE Item (
+                      id SERIAL PRIMARY KEY,
+                      quantity INTEGER NOT NULL,
+                      delivery_id BIGINT,
+                      product_name VARCHAR(255)
+);
+
+CREATE TABLE Product (
+                         id SERIAL PRIMARY KEY,
+                         quantity_stock INTEGER,
+                         value NUMERIC(10,2),
+                         volume FLOAT(53) NOT NULL,
+                         weight FLOAT(53) NOT NULL,
                          name VARCHAR(255),
-                         weight FLOAT NOT NULL,
-                         volume FLOAT NOT NULL,
-                         value NUMERIC(10,2)
+                         position_stock VARCHAR(255)
 );
 
-ALTER TABLE address
-    ADD CONSTRAINT fk_address_customer FOREIGN KEY (customer_id) REFERENCES customers (id);
-
-ALTER TABLE customers_email_list
-    ADD CONSTRAINT fk_customers_email_list FOREIGN KEY (customers_id) REFERENCES customers (id);
-
-ALTER TABLE delivery
-    ADD CONSTRAINT fk_product_delivery FOREIGN KEY (product_id) REFERENCES product (id);
-
-ALTER TABLE delivery
-    ADD CONSTRAINT fk_recipient_delivery FOREIGN KEY (recipient_id) REFERENCES customers (id);
-
-ALTER TABLE delivery
-    ADD CONSTRAINT fk_sender_delivery FOREIGN KEY (sender_id) REFERENCES customers (id);
+ALTER TABLE Customers ADD CONSTRAINT fk_customer_address  FOREIGN KEY (address_id) REFERENCES Address(id);
+ALTER TABLE Customers_Email_List ADD CONSTRAINT fk_customers_email_list FOREIGN KEY (customers_id) REFERENCES Customers(id);
+ALTER TABLE Delivery ADD CONSTRAINT fk_recipient_delivery FOREIGN KEY (recipient_id) REFERENCES Customers(id);
+ALTER TABLE Delivery ADD CONSTRAINT fk_sender_delivery FOREIGN KEY (sender_id) REFERENCES Customers(id);
+ALTER TABLE Item ADD CONSTRAINT fk_item_delivery FOREIGN KEY (delivery_id) REFERENCES Delivery(id);
