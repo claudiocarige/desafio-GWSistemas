6ec50dc9fdc9e1209ac24fe98c0b9bb4ea2de6b4
Claudio Carigé <ccarige@gmail.com>
2024-07-18 16:06:36 -0300

    [ addition ] : Inserted itemShippingValue attribute in item class

--- mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/domain/entities/Item.java
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/domain/entities/Item.java
@@ -1,8 +1,11 @@
 package br.com.claudiocarige.mspersistencedb.core.domain.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.math.BigDecimal;
 
 
 @Getter
@@ -18,6 +21,8 @@ public class Item {
     private Long id;
 
     private String productName;
+
+    private BigDecimal itemShippingValue;//Será calculado com os valores do produto
 
     private int quantity;
 
