a4307db3c6bfa46354e0c59c0036b56d015933e1
Claudio Carigé <ccarige@gmail.com>
2024-07-18 20:32:38 -0300

    [ addition ] : Created Query metods in CustomerRepository

--- mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/infra/persistence/repositories/postgresrepository/CustomerRepository.java
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/infra/persistence/repositories/postgresrepository/CustomerRepository.java
@@ -2,7 +2,22 @@ package br.com.claudiocarige.mspersistencedb.infra.persistence.repositories.post
 
 import br.com.claudiocarige.mspersistencedb.core.domain.entities.Customers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
-public interface CustomerRepository extends JpaRepository< Customers, Long> {
+public interface CustomerRepository extends JpaRepository< Customers, Long > {
+
+    boolean existsByPrincipalEmail( String principalEmail );
+
+    boolean existsByPhoneNumber( String phoneNumber );
+
+    @Query( "SELECT CASE WHEN COUNT(c) > 0 THEN TRUE ELSE FALSE END FROM CompanyCustomer c WHERE c.cnpj = :cnpj" )
+    boolean existsByCnpj( @Param( "cnpj" ) String cnpj );
+
+    boolean existsByCustomerName( String customerName );
+
+    @Query( "SELECT CASE WHEN COUNT(c) > 0 THEN TRUE ELSE FALSE END FROM IndividualCustomer c WHERE c.cpf = :cpf" )
+    boolean existsByCpf( @Param( "cpf" ) String cpf );
+
 }
