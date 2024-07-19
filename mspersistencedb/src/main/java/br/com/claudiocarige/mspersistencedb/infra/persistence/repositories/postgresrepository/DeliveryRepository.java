2e4b32e9bd68d51d24828798cf617b7601f1c343
Claudio Carigé <ccarige@gmail.com>
2024-07-19 10:42:55 -0300

    [ feat ] : Add queries for updating and searching for status in Delivery
    Add two custom queries to the Delivery entity:
    - Update delivery status by ID.
    - Search for delivery status by ID.

--- mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/infra/persistence/repositories/postgresrepository/DeliveryRepository.java
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/infra/persistence/repositories/postgresrepository/DeliveryRepository.java
@@ -1,8 +1,20 @@
 package br.com.claudiocarige.mspersistencedb.infra.persistence.repositories.postgresrepository;
 
 import br.com.claudiocarige.mspersistencedb.core.domain.entities.Delivery;
+import br.com.claudiocarige.mspersistencedb.core.domain.enums.DeliveryStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface DeliveryRepository extends JpaRepository< Delivery, Long > {
+
+    @Modifying
+    @Query( "UPDATE Delivery d SET d.statusDelivery = :statusDelivery WHERE d.id = :id" )
+    void updateStatusDelivery( @Param( "id" ) Long id, @Param( "statusDelivery" ) DeliveryStatus statusDelivery );
+
+    @Query( "SELECT d.statusDelivery FROM Delivery d WHERE d.id = :id" )
+    DeliveryStatus findStatusDeliveryById( @Param( "id" ) Long id );
+
 }
