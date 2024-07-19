282e1a85b391a192ce178420d66dd58a9124e22e
Claudio Carigé <ccarige@gmail.com>
2024-07-19 10:39:19 -0300

    [ refactor ] : Propagates MessagingException in requestDelivery
    The requestDelivery method now throws MessagingException to indicate possible errors related to sending messages.

--- mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/usecases/DeliveryService.java
+++ mspersistencedb/src/main/java/br/com/claudiocarige/mspersistencedb/core/usecases/DeliveryService.java
@@ -5,15 +5,16 @@ import br.com.claudiocarige.mspersistencedb.core.domain.enums.DeliveryStatus;
 import br.com.claudiocarige.mspersistencedb.core.dtos.DeliveryDTO;
 import br.com.claudiocarige.mspersistencedb.core.dtos.RequestDelivery;
 import br.com.claudiocarige.mspersistencedb.core.dtos.ResponseOfSolicitation;
+import jakarta.mail.MessagingException;
 
 import java.util.List;
 
 
 public interface DeliveryService {
 
-    ResponseOfSolicitation requestDelivery( RequestDelivery requestDelivery ); //Solicitar Entrega
+    ResponseOfSolicitation requestDelivery( RequestDelivery requestDelivery ) throws MessagingException;
 
-    DeliveryStatus carryOutDelivery( String passwordDelivery, Long deliveryId ); //Realizar entrega
+    DeliveryDTO carryOutDelivery( String passwordDelivery, Long deliveryId ) throws MessagingException;
 
     Boolean CheckIfDeliveryIsCompleted( Long deliveryId );
 
